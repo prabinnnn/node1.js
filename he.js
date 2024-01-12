8e3257c9d4cd7959d1ba41b3f77b279c7ccb1bce
prabinnnn <prabinbhandari120@gmail.com>
2024-01-12 16:25:46 +0545

    add he

--- he.js
+++ he.js
@@ -3,8 +3,8 @@ const libaray = require("./libaray/fun");
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
-    const modul = write("hello world");
-    const modul1 = libaray.convertString("heloo prabin");
+    let result = libaray.convertString("heloo form nepal");
+    res.end(result);
   })
   .listen(8000);
 console.log("Server is running on http://localhost:8000/");
