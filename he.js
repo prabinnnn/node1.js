61453d2fd560f30c785456fef191938d52f486a6
prabinnnn <prabinbhandari120@gmail.com>
2024-01-12 15:54:31 +0545

    add fun

--- he.js
+++ he.js
@@ -1,11 +1,9 @@
 const http = require("http");
 http
   .createServer((req, res) => {
-    get;
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write("hello to me");
     res.end();
   })
   .listen(8000);
-
 console.log("Server is running on http://localhost:8000/");
