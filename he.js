9940c1a04150ceab119ca127b0579414fc96822b
prabinnnn <prabinbhandari120@gmail.com>
2024-01-12 16:28:59 +0545

    add he

--- he.js
+++ he.js
@@ -1,10 +1,12 @@
 const http = require("http");
 const libaray = require("./libaray/fun");
+const toProperUpper = require("proper-upper-case");
 http
   .createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
     let result = libaray.convertString("heloo form nepal");
-    res.end(result);
+    let result1 = toProperUpper("tidnedjdnjd");
+    res.end(result + result1);
   })
   .listen(8000);
 console.log("Server is running on http://localhost:8000/");
